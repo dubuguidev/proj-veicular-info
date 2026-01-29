17ccd2d68f1757e411a68a9dca16b487bfe433a5
Dubugui <gabrielbcostadev@gmail.com>
2026-01-29 09:27:23 -0300

    correção1

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,46 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+
+  veiculos: any[] = [];
+
+  novoVeiculo = {
+    placa: '',
+    modelo: '',
+    marca: '',
+    ano: null,
+    chassi: '',
+    renavam: ''
+  };
+
+  salvarVeiculo() {
+    const novo = {
+      id: Date.now(),
+      ...this.novoVeiculo
+    };
+
+    this.veiculos.push(novo);
+
+    this.novoVeiculo = {
+      placa: '',
+      modelo: '',
+      marca: '',
+      ano: null,
+      chassi: '',
+      renavam: ''
+    };
+  }
+
+  deletarVeiculo(id: number) {
+    this.veiculos = this.veiculos.filter(v => v.id !== id);
+  }
 }
